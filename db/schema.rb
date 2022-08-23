14bd28c47d6acd1cf3ca43ce351caaf38ff66b39
Alfred Boateng <programmingwithalfred@gmail.com>
2022-08-23 12:29:23 +0000

    create subject entity

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_23_114411) do
+ActiveRecord::Schema.define(version: 2022_08_23_122131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,12 @@ ActiveRecord::Schema.define(version: 2022_08_23_114411) do
   create_table "classrooms", force: :cascade do |t|
     t.string "name", null: false
     t.integer "role", default: 0, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "subjects", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
