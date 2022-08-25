000d516f478339d5c4a0180ed207f5a7283407d1
Alfred Boateng <programmingwithalfred@gmail.com>
2022-08-25 16:28:20 +0000

    Add user profile update and user list on UI

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[ show edit update destroy ]
+
+  def index
+    @title = "Users"
+    @users = User.all.order(created_at: :desc)
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to user_url(@user), notice: "user was successfully updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+
+  def user_params
+      params.require(:user).permit(:role, :name)
+    end
+
+end
