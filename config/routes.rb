d40d58adbdb0b70d7b5ba4b4e814cf5108463e13
Alfred Boateng <programmingwithalfred@gmail.com>
2022-08-26 13:49:59 +0000

    Add error handler and pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'errors/not_found'
+  get 'errors/internal_server'
+  get 'errors/unprocessable'
+  get '/404', to: 'errors#not_found', via: :all
+  get '/500', to: 'errors#internal_server', via: :all
+  get '/422', to: 'errors#unprocessable', via: :all
+
   resources :comments, only: [:create, :new]
   resources :courses
   resources :students
