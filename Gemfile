38a2b4c3036451e0b5c021373954cb9b1bc9b0b5
Alfred Boateng <programmingwithalfred@gmail.com>
2022-08-27 09:17:32 +0000

    Remove tailwind

--- Gemfile
+++ Gemfile
@@ -53,7 +53,6 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
-gem "tailwindcss-rails", "~> 2.0"
 gem 'net-smtp', require: false
 gem 'net-imap', require: false
 gem 'net-pop', require: false
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,29 +1,4 @@
 require "active_support/core_ext/integer/time"
-require 'sprockets/sass_compressor'
-class Sprockets::SassCompressor
-  TAILWIND_SEARCH = "--tw-".freeze
-  def call(*args)
-    input = if defined?(data)
-              data # sprockets 2.x
-            else
-              args[0][:data] #sprockets 3.x
-            end
-
-    return input if skip_compiling?(input) # added this line
-
-    SassC::Engine.new(
-      input,
-      {
-        style: :compressed
-      }
-    ).render
-  end
-
-  def skip_compiling?(body)
-    body.include?(TAILWIND_SEARCH)
-  end
-
-end
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
