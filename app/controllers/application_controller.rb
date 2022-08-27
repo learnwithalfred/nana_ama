d53a7d5d5eac736cdf66bfaafae3bf5fbf46ffaa
Alfred Boateng <programmingwithalfred@gmail.com>
2022-08-27 10:09:14 +0000

    Add test for for errors controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
   layout :set_layout
   before_action :configure_permitted_parameters, if: :devise_controller?
 
--- test/controllers/errors_controller_test.rb
+++ test/controllers/errors_controller_test.rb
@@ -3,16 +3,16 @@ require "test_helper"
 class ErrorsControllerTest < ActionDispatch::IntegrationTest
   test "should get not_found" do
     get errors_not_found_url
-    assert_response :success
+    assert_response :not_found
   end
 
   test "should get internal_server" do
     get errors_internal_server_url
-    assert_response :success
+    assert_response :internal_server_error
   end
 
   test "should get unprocessable" do
     get errors_unprocessable_url
-    assert_response :success
+    assert_response :unprocessable_entity
   end
 end
